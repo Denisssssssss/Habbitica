b2f788caca1df954f8dfa99613d1342248f81ba4
Bulat Vafin <vafin20@gmail.com>
2021-07-02 02:24:58 +0300

    bug fixes

--- src/main/java/com/simbirsoft/habbitica/impl/controllers/RootController.java
+++ src/main/java/com/simbirsoft/habbitica/impl/controllers/RootController.java
@@ -1,5 +1,7 @@
 package com.simbirsoft.habbitica.impl.controllers;
 
+import com.simbirsoft.habbitica.impl.security.details.UserDetailsImpl;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class RootController {
 
     @GetMapping("/")
-    public String root(Model model) {
+    public String root(@AuthenticationPrincipal UserDetailsImpl userDetails, Model model) {
 
         model.addAttribute("first_footer", 1);
-
+        model.addAttribute("user", userDetails.getUser());
         return "index";
     }
 }
